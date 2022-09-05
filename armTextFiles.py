75e501ebdf94656048bf54e9dfb078abed5764c4
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-09-05 12:36:12 -0400

    Update and rename textFiles.py to armTextFiles.py

--- armTextFiles.py
+++ armTextFiles.py
@@ -1,6 +1,6 @@
 import os
 try:
-    os.mkdir("textFiles")
+    os.mkdir("armTextFiles")
 except OSError as error:
     pass
     
@@ -13,6 +13,6 @@ for i in range(os.stat("arm9.bin").st_size):
 starts.append((os.stat("arm9.bin").st_size - 640068) + 602876)
 
 for i in range(len(starts) - 1):
-    new = open("textFiles/" + str(starts[i]) + ".bin", "wb")
+    new = open("armTextFiles/" + str(starts[i]) + ".bin", "wb")
     new.write(binn[starts[i]:starts[i + 1]])
     new.close()
