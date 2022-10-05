e25e5f513fbbc3376e84d30ae4464113edc48ce6
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-10-05 19:39:32 -0400

    files are BMG now

--- bringTogether.py
+++ bringTogether.py
@@ -11,23 +11,53 @@ for i in range(34):
     file.close()
     offsetList = []
     for j in range(len(lines) - 1):
-        if lines[j + 1].startswith("msgid") and (len(lines[j + 1]) > 14) and ("<" not in lines[j + 1]):
-             offsetList.append(int(lines[j].split("<- ")[1].split(" ")[0]))   
-    offsetList.sort()
+        if lines[j + 1].startswith("msgid") and (len(lines[j + 1]) > 14) and ("<" not in lines[j + 1]) and ("@" not in lines[j + 1]) and ("ï" not in lines[j + 1]):
+            offsetList.append([ int(lines[j].split("(")[1].split(")")[0], 16), int(lines[j].split("<- ")[1].split(" ")[0], 16),
+                int(lines[j].split("<- ")[1].split(" ")[0], 16) + 4 ])   
+    offsetList.sort(key = lambda a: a[0])
 
+    starter = 0
     if (os.path.exists("./" + str(i).zfill(4) + "_textFiles/") == True):
         for root, dirs, files in os.walk("./" + str(i).zfill(4) + "_textFiles/"):
             for file in files:
-                oldOffset = int(file.split("_")[0])
-                change = os.stat(os.path.join(root, file)).st_size - (int(file.split("_")[1]) - int(file.split("_")[0]))
-                for j in range(len(offsetList):
-                    if (j > oldOffset):
-                        offsetList.insert(j, -1)
-                        offsetList.insert(j + 1, change)
+                begOffset = int(file.split("_")[0])
+                endOffset = int(file.split("_")[1].split(".")[0])
+                change = os.stat(os.path.join(root, file)).st_size - (endOffset - begOffset)
+                for j in range(starter, len(offsetList)):
+                    if (offsetList[j][0] > begOffset):
+                        offsetList.insert(j, [ -1, begOffset, endOffset ])
+                        globalSum = globalSum + change
+                        starter = j + 1
                         break
-
-
-    new = open("./output/overlay9_" + str(i).zfill(4), "wb")
+                    else:
+                        offsetList[j][0] = offsetList[j][0] + globalSum
+    
+    offsetList.sort(key = lambda a: a[1])
+    old = open("./overlay9_" + str(i).zfill(4) + ".bin", "rb")
+    whole = old.read()
+    old.close()
+    new = open("./output/overlay9_" + str(i).zfill(4) + ".bin", "wb")
     new.close()
-    new = open("./output/overlay9_" + str(i).zfill(4), "ab")
+    new = open("./output/overlay9_" + str(i).zfill(4) + ".bin", "ab")
+
+    if (len(offsetList) > 0):
+        new.write(whole[0:offsetList[0][1]])
+        new.write(offsetList[0][0].to_bytes(4, "little"))
+        for j in range(1, len(offsetList)):
+            if (offsetList[j][0] != -1):
+                new.write(whole[offsetList[j - 1][2]:offsetList[j][1]])
+                new.write(offsetList[j][0].to_bytes(4, "little"))
+            else:
+                temp = open("./" + str(i).zfill(4) + "_textFiles/" + str(offsetList[j][1]) + "_" + str(offsetList[j][2]) + ".BMG", "rb")
+                new.write(whole[offsetList[j - 1][2]:offsetList[j][1]])
+                new.write(temp.read())
+                temp.close()
+        new.write(whole[offsetList[-1][2]:])
+        new.close()
+    else:
+        new.write(whole)
+        new.close()
+            
+        
+    
     
