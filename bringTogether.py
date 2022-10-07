e3816f76f892536fc9355a761f1fffe0a34eacc8
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-10-07 10:35:21 -0400

    added bmg pointer table manip

--- bringTogether.py
+++ bringTogether.py
@@ -4,76 +4,7 @@ try:
     os.mkdir("output")
 except OSError as error:
     pass
-
-globalSum = 0
-
-file = open("./overlay_pointers/arm9_TABLE.po", "rt")
-lines = file.read().split("\n")
-file.close()
-offsetList = []
-for j in range(len(lines) - 1):
-    if lines[j + 1].startswith("msgid") and (len(lines[j + 1]) > 14) and ("<" not in lines[j + 1]) and ("@" not in lines[j + 1]) and ("ï" not in lines[j + 1]):
-        if len(lines[j].split(",")) > 1:
-            print(j)
-        for substring in lines[j].split(";"):
-            offsetList.append([ int(substring.split("(")[1].split(")")[0], 16), int(substring.split("<- ")[1].split(" ")[0], 16),
-                int(substring.split("<- ")[1].split(" ")[0], 16) + 4 ])   
-offsetList.sort(key = lambda a: a[0])
-
-starter = 0
-for root, dirs, files in os.walk("./arm9TextFiles/"):
-    for file in files:
-        if (file.endswith(".BMG") == True):
-            subprocess.run([ "./wbmgt/wbmgt.exe", "encode", os.path.join(root, file), "--overwrite", "--encoding", "SHIFTJIS" ])
-            begOffset = int(file.split("_")[0])
-            endOffset = int(file.split("_")[1].split(".")[0])
-            change = os.stat("./arm9TextFiles/" + file).st_size - (endOffset - begOffset)
-            if (change != 0):
-                print("arm9: " + file)
-            for j in range(starter, len(offsetList)):
-                if (offsetList[j][0] > begOffset):
-                    offsetList.insert(j, [ -1, begOffset, endOffset ])
-                    globalSum = globalSum + change
-                    starter = j + 1
-                    break
-                elif (j == len(offsetList) - 1):
-                    offsetList.append([ -1, begOffset, endOffset ])
-                    globalSum = globalSum + change
-                    starter = -1
-                else:
-                    offsetList[j][0] = offsetList[j][0] + globalSum
-if (starter != -1):
-    for j in range(starter, len(offsetList)):
-        if (offsetList[j][0] != -1):
-            offsetList[j][0] = offsetList[j][0] + globalSum
-
-offsetList.sort(key = lambda a: a[1])
-old = open("./arm9.bin", "rb")
-whole = old.read()
-old.close()
-new = open("./output/arm9.bin", "wb")
-new.close()
-new = open("./output/arm9.bin", "ab")
-
-if (len(offsetList) > 0):
-    new.write(whole[0:offsetList[0][1]])
-    new.write(offsetList[0][0].to_bytes(4, "little"))
-    for j in range(1, len(offsetList)):
-        if (offsetList[j][0] != -1):
-            new.write(whole[offsetList[j - 1][2]:offsetList[j][1]])
-            new.write(offsetList[j][0].to_bytes(4, "little"))
-        else:
-            temp = open("./arm9TextFiles/" + str(offsetList[j][1]) + "_" + str(offsetList[j][2]) + ".BMG", "rb")
-            new.write(whole[offsetList[j - 1][2]:offsetList[j][1]])
-            new.write(temp.read())
-            temp.close()
-    new.write(whole[offsetList[-1][2]:])
-    new.close()
-else:
-    new.write(whole)
-    new.close()
-
-
+fileDict = {}
 
 for i in range(34):
     globalSum = 0
@@ -100,6 +31,7 @@ for i in range(34):
                     begOffset = int(file.split("_")[0])
                     endOffset = int(file.split("_")[1].split(".")[0])
                     change = os.stat("./" + str(i).zfill(4) + "_textFiles/" + file).st_size - (endOffset - begOffset)
+                    fileDict[begOffset + int(0x021275E0)] = globalSum
                     if (change != 0):
                         print("overlay " + str(i) + ": " + file + "  change = " + str(change))
                     for j in range(starter, len(offsetList)):
@@ -147,4 +79,74 @@ for i in range(34):
             
         
     
-    
+globalSum = 0
+
+file = open("./overlay_pointers/arm9_TABLE.po", "rt")
+lines = file.read().split("\n")
+file.close()
+offsetList = []
+for j in range(len(lines) - 1):
+    if lines[j + 1].startswith("msgid") and (len(lines[j + 1]) > 14) and ("<" not in lines[j + 1]) and ("@" not in lines[j + 1]) and ("ï" not in lines[j + 1]):
+        if ("MESGbmg1" not in lines[j + 1]):
+            for substring in lines[j].split(";"):
+                offsetList.append([ int(substring.split("(")[1].split(")")[0], 16), int(substring.split("<- ")[1].split(" ")[0], 16),
+                    int(substring.split("<- ")[1].split(" ")[0], 16) + 4 ])   
+offsetList.sort(key = lambda a: a[0])
+
+starter = 0
+for root, dirs, files in os.walk("./arm9TextFiles/"):
+    for file in files:
+        if (file.endswith(".BMG") == True):
+            subprocess.run([ "./wbmgt/wbmgt.exe", "encode", os.path.join(root, file), "--overwrite", "--encoding", "SHIFTJIS" ])
+            begOffset = int(file.split("_")[0])
+            endOffset = int(file.split("_")[1].split(".")[0])
+            change = os.stat("./arm9TextFiles/" + file).st_size - (endOffset - begOffset)
+            fileDict[begOffset + int(0x02000000)] = globalSum
+            if (change != 0):
+                print("arm9: " + file)
+            for j in range(starter, len(offsetList)):
+                if (offsetList[j][0] > begOffset):
+                    offsetList.insert(j, [ -1, begOffset, endOffset ])
+                    globalSum = globalSum + change
+                    starter = j + 1
+                    break
+                elif (j == len(offsetList) - 1):
+                    offsetList.append([ -1, begOffset, endOffset ])
+                    globalSum = globalSum + change
+                    starter = -1
+                else:
+                    offsetList[j][0] = offsetList[j][0] + globalSum
+if (starter != -1):
+    for j in range(starter, len(offsetList)):
+        if (offsetList[j][0] != -1):
+            offsetList[j][0] = offsetList[j][0] + globalSum
+
+old = open("./arm9.bin", "rb")
+whole = old.read()
+old.close()
+new = open("./output/arm9.bin", "wb")
+new.close()
+new = open("./output/arm9.bin", "ab")
+
+for i in range(int(0x0932FC), int(0x093458), 4):
+    pointed = int.from_bytes(whole[i:(i + 4)], "little")
+    offsetList.append([ pointed + fileDict[pointed], i, i + 4 ]) 
+
+offsetList.sort(key = lambda a: a[1])
+if (len(offsetList) > 0):
+    new.write(whole[0:offsetList[0][1]])
+    new.write(offsetList[0][0].to_bytes(4, "little"))
+    for j in range(1, len(offsetList)):
+        if (offsetList[j][0] != -1):
+            new.write(whole[offsetList[j - 1][2]:offsetList[j][1]])
+            new.write(offsetList[j][0].to_bytes(4, "little"))
+        else:
+            temp = open("./arm9TextFiles/" + str(offsetList[j][1]) + "_" + str(offsetList[j][2]) + ".BMG", "rb")
+            new.write(whole[offsetList[j - 1][2]:offsetList[j][1]])
+            new.write(temp.read())
+            temp.close()
+    new.write(whole[offsetList[-1][2]:])
+    new.close()
+else:
+    new.write(whole)
+    new.close()   
